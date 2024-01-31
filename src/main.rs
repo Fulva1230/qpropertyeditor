218618ced0aa3e6e63faf52fc4d15cf54e822136
Chill <b10503137@gapps.ntust.edu.tw>
2024-01-31 21:27:54 +0800

    optional setter and notifier

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use convert_case::{Case, Casing};
 
 slint::slint! {
+    import { CheckBox } from "std-widgets.slint";
     export component MainWindow inherits Window{
         title: "QPropertyEditor";
         default-font-size: 13pt;
@@ -9,12 +10,33 @@ slint::slint! {
         in property <string> declarationText;
         out property <string> valueType: typeTextInput.text;
         out property <string> valueName: nameTextInput.text;
+        out property <bool> settable: settableCheck.checked;
+        out property <bool> notifiable: notifiableCheck.checked;
         property<length> widthOfDisplay: 80pt;
         max-height: mainLayout.min-height;
 
         mainLayout := GridLayout {
             spacing: 5px;
             padding: 5px;
+            Row {
+                settableCheck := CheckBox {
+                    text: "Settable";
+                    checked: true;
+                    toggled => {
+                        root.generateProperty()
+                    }
+                }
+                notifiableCheck := CheckBox {
+                    text: "Notifiable";
+                    checked: true;
+                    toggled => {
+                        root.generateProperty()
+                    }
+                }
+                Rectangle {
+                    horizontal-stretch: 1;
+                }
+            }
             Row {
                 Text {
                     text: "Type: ";
@@ -63,13 +85,22 @@ fn main() -> Result<(), Box<dyn Error>> {
             let the_type = mainWindow.get_valueType();
             let the_name = mainWindow.get_valueName();
             mainWindow.set_declarationText(
-                std::format!("Q_PROPERTY({} {} READ {} WRITE {} NOTIFY {})",
+                std::format!("Q_PROPERTY({} {} READ {}{}{})",
                              the_type,
                              the_name,
                              the_name,
-                             std::format!("set_{}", the_name).to_case(Case::Camel),
-                             std::format!("{}Changed", the_name).to_case(Case::Camel)
-                ).into())
+                             if mainWindow.get_settable() {
+                                 std::format!(" WRITE {}", std::format!("set_{}", the_name).to_case(Case::Camel))
+                             } else {
+                                 String::new()
+                             },
+                             if mainWindow.get_notifiable() {
+                                 std::format!(" NOTIFY {}", std::format!(" NOTIFY {}Changed", the_name).to_case(Case::Camel))
+                             } else {
+                                 String::new()
+                             }
+                ).into()
+            )
         }
     });
     main_window.invoke_generateProperty();
