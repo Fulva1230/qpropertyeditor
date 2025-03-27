97d0b88f7203895cdb6397360b9009d3de8b1b16
Chill <b10503137@gapps.ntust.edu.tw>
2025-03-27 22:52:18 +0800

    copy generated string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![windows_subsystem = "windows"]
+use arboard::Clipboard;
 use convert_case::{Case, Casing};
 use slint::SharedString;
 use std::error::Error;
@@ -15,6 +16,10 @@ slint::slint! {
         default-font-size: 13pt;
         callback save;
         callback generateProperty;
+        callback copyDeclaration;
+        callback copyGetter;
+        callback copySetter;
+        callback copyNotifier;
         in property <string> declarationText;
         in property <string> getterText;
         in property <string> setterText;
@@ -111,6 +116,12 @@ slint::slint! {
                 }
             }
             HorizontalBox {
+                Button {
+                    text: "📑";
+                    clicked() => {
+                        root.copyDeclaration()
+                    }
+                }
                 Text {
                     text: "Declaration: ";
                     min-width: widthOfDisplay;
@@ -122,6 +133,12 @@ slint::slint! {
                 }
             }
             HorizontalBox {
+                Button {
+                    text: "📑";
+                    clicked() => {
+                        root.copyGetter()
+                    }
+                }
                 Text {
                     text: "Getter: ";
                     min-width: widthOfDisplay;
@@ -133,6 +150,12 @@ slint::slint! {
                 }
             }
             if settableCheck.checked: HorizontalBox {
+                Button {
+                    text: "📑";
+                    clicked() => {
+                        root.copySetter()
+                    }
+                }
                 Text {
                     text: "Setter: ";
                     min-width: widthOfDisplay;
@@ -144,6 +167,12 @@ slint::slint! {
                 }
             }
             if notifiableCheck.checked: HorizontalBox {
+                Button {
+                    text: "📑";
+                    clicked() => {
+                        root.copyNotifier()
+                    }
+                }
                 Text {
                     text: "Notifier: ";
                     min-width: widthOfDisplay;
@@ -232,52 +261,79 @@ impl QProperty {
     }
 }
 
+impl From<&TheMainWindow> for QProperty {
+    fn from(value: &TheMainWindow) -> Self {
+        QProperty {
+            the_type: value.get_valueType(),
+            the_name: value.get_valueName(),
+            settable: value.get_settable(),
+            notifiable: value.get_notifiable(),
+            const_ref: value.get_constRef(),
+        }
+    }
+}
+
 fn on_save_callback(main_window: TheMainWindow) {
     if let Ok(mut file) = fs::File::create("save.txt") {
-        let qproperty = QProperty {
-            the_type: main_window.get_valueType(),
-            the_name: main_window.get_valueName(),
-            settable: main_window.get_settable(),
-            notifiable: main_window.get_notifiable(),
-            const_ref: main_window.get_constRef(),
-        };
+        let qproperty: QProperty = (&main_window).into();
         file.write_all(qproperty.summery().as_bytes()).unwrap()
     }
 }
 
 fn on_generate_property(main_window: TheMainWindow) {
-    let qproperty = QProperty {
-        the_type: main_window.get_valueType(),
-        the_name: main_window.get_valueName(),
-        settable: main_window.get_settable(),
-        notifiable: main_window.get_notifiable(),
-        const_ref: main_window.get_constRef(),
-    };
+    let qproperty: QProperty = (&main_window).into();
     main_window.set_declarationText(qproperty.declaration().into());
     main_window.set_getterText(qproperty.getter().into());
     main_window.set_setterText(qproperty.setter().into());
     main_window.set_notifierText(qproperty.notifier().into());
 }
+fn on_copy_declaration(main_window: TheMainWindow) -> Result<(), Box<dyn Error>> {
+    let qproperty: QProperty = (&main_window).into();
+    let mut clipboard = Clipboard::new()?;
+    clipboard.set_text(qproperty.declaration())?;
+    Ok(())
+}
 
+fn on_copy_getter(main_window: TheMainWindow) -> Result<(), Box<dyn Error>> {
+    let qproperty: QProperty = (&main_window).into();
+    let mut clipboard = Clipboard::new()?;
+    clipboard.set_text(qproperty.getter())?;
+    Ok(())
+}
+
+fn on_copy_setter(main_window: TheMainWindow) -> Result<(), Box<dyn Error>> {
+    let qproperty: QProperty = (&main_window).into();
+    let mut clipboard = Clipboard::new()?;
+    clipboard.set_text(qproperty.setter())?;
+    Ok(())
+}
+
+fn on_copy_notifier(main_window: TheMainWindow) -> Result<(), Box<dyn Error>> {
+    let qproperty: QProperty = (&main_window).into();
+    let mut clipboard = Clipboard::new()?;
+    clipboard.set_text(qproperty.notifier())?;
+    Ok(())
+}
+
+fn callback<T>(
+    main_window: &TheMainWindow,
+    functor: impl Fn(TheMainWindow) -> T + 'static
+) -> impl FnMut() -> () + 'static{
+    let main_window_wk_ref = main_window.as_weak();
+    move || {
+        if let Some(main_window) = main_window_wk_ref.upgrade() {
+            functor(main_window);
+        }
+    }
+}
 fn main() -> Result<(), Box<dyn Error>> {
     let main_window = TheMainWindow::new()?;
-
-    main_window.on_save({
-        let main_window_wk_ref = main_window.as_weak();
-        move || {
-            if let Some(main_window) = main_window_wk_ref.upgrade() {
-                on_save_callback(main_window)
-            }
-        }
-    });
-    main_window.on_generateProperty({
-        let main_window_wk_ref = main_window.as_weak();
-        move || {
-            if let Some(main_window) = main_window_wk_ref.upgrade() {
-                on_generate_property(main_window);
-            }
-        }
-    });
+    main_window.on_save(callback(&main_window, on_save_callback));
+    main_window.on_generateProperty(callback(&main_window, on_generate_property));
+    main_window.on_copyDeclaration(callback(&main_window, on_copy_declaration));
+    main_window.on_copyGetter(callback(&main_window, on_copy_getter));
+    main_window.on_copySetter(callback(&main_window, on_copy_setter));
+    main_window.on_copyNotifier(callback(&main_window, on_copy_notifier));
     main_window.invoke_generateProperty();
     main_window.run()?;
     Ok(())
